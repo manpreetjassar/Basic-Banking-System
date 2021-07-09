6c55466df96a2c8bfa9bc2e52a1eb68e3de07215
manpreetjassar <53909120+manpreetjassar@users.noreply.github.com>
2021-07-10 01:02:38 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="customer";
+//Development connection
+//$servername="localhost";
+//$username="root";
+//$password="";
+//$dbname="customer";
+
+//Remote database connection
+$servername="remotemysql.com";
+$username="8XLDOMMr8I";
+$password="dSbuEy52S0";
+$dbname="8XLDOMMr8I";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
